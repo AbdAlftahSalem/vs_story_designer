b76ad455049c80d15f02919925490167394cb65d
AbdAlftahSalem <abd321alftah@gmail.com>
2025-07-13 16:16:24 +0300

    Solve bug

--- lib/src/presentation/utils/modal_sheets.dart
+++ lib/src/presentation/utils/modal_sheets.dart
@@ -2,19 +2,15 @@
 
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-
 // import 'package:modal_gif_picker/modal_gif_picker.dart';
 import 'package:provider/provider.dart';
-
 // import 'package:vs_story_designer/src/domain/models/editable_items.dart';
 import 'package:vs_story_designer/src/domain/providers/notifiers/control_provider.dart';
 import 'package:vs_story_designer/src/domain/providers/notifiers/draggable_widget_notifier.dart';
 import 'package:vs_story_designer/src/domain/providers/notifiers/painting_notifier.dart';
 import 'package:vs_story_designer/src/domain/providers/notifiers/text_editing_notifier.dart';
-
 // import 'package:vs_story_designer/src/domain/sevices/save_as_image.dart';
 import 'package:vs_story_designer/src/presentation/utils/Extensions/hexColor.dart';
-
 // import 'package:vs_story_designer/src/presentation/utils/constants/item_type.dart';
 import 'package:vs_story_designer/src/presentation/widgets/animated_onTap_button.dart';
 import 'package:vs_story_designer/vs_story_designer.dart';
@@ -88,7 +84,7 @@ Future<bool> exitDialog({
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
                   Text(
-                    'Discard Edits?',
+                    title,
                     style: TextStyle(
                         fontSize: 22,
                         fontWeight: FontWeight.w600,
@@ -101,7 +97,7 @@ Future<bool> exitDialog({
                     height: 20,
                   ),
                   Text(
-                    "If you go back now, you'll lose all the edits you've made.",
+                    description,
                     style: TextStyle(
                         fontSize: 15,
                         fontWeight: FontWeight.w400,
@@ -128,7 +124,7 @@ Future<bool> exitDialog({
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Text(
-                          'Discard',
+                          discardText,
                           style: TextStyle(
                               fontSize: 16,
                               color: Colors.redAccent.shade200,
@@ -197,7 +193,7 @@ Future<bool> exitDialog({
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Text(
-                          'Cancel',
+                          cancelText,
                           style: TextStyle(
                               fontSize: 16,
                               color: themeType == ThemeType.light
